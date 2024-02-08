218a1e4c0d6c9d9f1ad2c3b271d601999de2411d
Y-T10 <t06t06ca2@gmail.com>
2024-02-09 00:10:25 +0900

    Buttonをwidget空間下に移動

--- include/outline-ui/widget/button.hpp
+++ include/outline-ui/widget/button.hpp
@@ -4,37 +4,39 @@
 #include "../OUI_widget_concept.hpp"
 #include "../OUI_types.hpp"
 
-template <class coord_type>
-struct Button {
-    using boundary_type = outlineUI::boundary_t<coord_type>;
+namespace outlineUI::widget {
+    template <class coord_type>
+    struct Button {
+        using boundary_type = outlineUI::boundary_t<coord_type>;
 
-    std::string text;
-    boundary_type button_area;
-    bool is_pressed;
+        std::string text;
+        boundary_type button_area;
+        bool is_pressed;
 
-    const boundary_type boundary_area() noexcept {
-        return button_area;
-    }
+        const boundary_type boundary_area() noexcept {
+            return button_area;
+        }
 
-    const boundary_type boundary_area() const noexcept {
-        return button_area;
-    }
+        const boundary_type boundary_area() const noexcept {
+            return button_area;
+        }
 
-    void boundary_area(const boundary_type& area) noexcept {
-        button_area = area;
-    }
+        void boundary_area(const boundary_type& area) noexcept {
+            button_area = area;
+        }
 
-    const bool pressed() const {
-        return is_pressed;
-    }
+        const bool pressed() const {
+            return is_pressed;
+        }
 
-    void push() {
-        is_pressed = true;
-    }
+        void push() {
+            is_pressed = true;
+        }
 
-    void release() {
-        is_pressed = false;
-    }
-};
-
-static_assert(outlineUI::Widget<Button<int>, int>);
+        void release() {
+            is_pressed = false;
+        }
+    };
+    
+    static_assert(outlineUI::Widget<Button<int>, int>);
+}
