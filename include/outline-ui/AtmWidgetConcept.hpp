6c7a0c45449bd047404c5ed36b2624b75cafbeb3
Y-T10 <t06t06ca2@gmail.com>
2024-02-08 23:32:28 +0900

    型とコンセプトをoutlineUI名前空間に入れた

--- include/outline-ui/AtmWidgetConcept.hpp
+++ include/outline-ui/AtmWidgetConcept.hpp
@@ -6,11 +6,13 @@
 
 #include "AtmTypes.hpp"
 
-template<class T, class coord_type>
-concept Widget2 = requires(T& w) {
-    // 境界を返すbundary_area関数を要求する
-    // const関数も定義する
-    { w.boundary_area() } -> std::same_as<const boundary_t<coord_type>>;
-    { std::as_const(w).boundary_area() } -> std::same_as<const boundary_t<coord_type>>;
-    w.boundary_area(boundary_t<coord_type>{});
-};
+namespace outlineUI {
+    template<class T, class coord_type>
+    concept Widget2 = requires(T& w) {
+        // 境界を返すbundary_area関数を要求する
+        // const関数も定義する
+        { w.boundary_area() } -> std::same_as<const boundary_t<coord_type>>;
+        { std::as_const(w).boundary_area() } -> std::same_as<const boundary_t<coord_type>>;
+        w.boundary_area(boundary_t<coord_type>{});
+    };
+}
